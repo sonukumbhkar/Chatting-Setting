b31baa6cc4c815c063f769edf6dafa3d4c2e782c
Sonu kumbhkar <59777243+sonukumbhkar@users.noreply.github.com>
2021-08-08 23:58:15 +0530

    2

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '6u*h^$w1ielwjw%797zzr8**hmbh@!_mc(f7p@qh6x$e69zufy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Will Turn  False when ready for deployment
+DEBUG = False  # Will Turn  False when ready for deployment
 
 ALLOWED_HOSTS = ['localhost',   "127.0.0.1"]
 
